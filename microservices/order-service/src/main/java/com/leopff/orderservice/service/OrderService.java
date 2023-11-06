ab3693c52bd88929e6f115873f7c78423556b85d
leopff <leonardo.freitas@gmail.com>
2023-11-06 15:22:55 -0300

    datacleaning

--- microservices/order-service/src/main/java/com/leopff/orderservice/service/OrderService.java
+++ microservices/order-service/src/main/java/com/leopff/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.leopff.orderservice.service;
 
+import com.leopff.orderservice.dto.InventoryResponse;
 import com.leopff.orderservice.dto.OrderLineItemsDto;
 import com.leopff.orderservice.dto.OrderRequest;
 import com.leopff.orderservice.model.Order;
@@ -10,7 +11,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -21,6 +24,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -33,8 +37,27 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
-        log.info("Order {} created", order.getId());
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        //if product in stock at InventoryService, place order
+        InventoryResponse[] inventoryResponseArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
+                .allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock) {
+            orderRepository.save(order);
+            log.info("Order {} created", order.getId());
+        } else {
+            throw new IllegalArgumentException("Product not in stock");
+        }
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
